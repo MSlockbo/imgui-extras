b7619150ad356bdb68552e98db67ff6ede91119d
Maddie Slockbower <mslockbo@gmail.com>
2024-11-03 12:55:25 -0500

    Updated License

--- imgui_extras.h
+++ imgui_extras.h
@@ -1,16 +1,19 @@
 // =====================================================================================================================
-// Copyright 2024 Medusa Slockbower
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//	imgui-extras, an extension for Dear ImGui that adds extra functionality.
+//	Copyright (C) 2024  Medusa Slockbower
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =====================================================================================================================
 
 #ifndef IMGUI_EXTRAS_H
@@ -20,18 +23,18 @@
 
 namespace ImGui
 {
-    bool InputUInt(const char* label,  unsigned int* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
-    bool InputUInt2(const char* label, unsigned int v[2], ImGuiInputTextFlags flags = 0);
-    bool InputUInt3(const char* label, unsigned int v[3], ImGuiInputTextFlags flags = 0);
-    bool InputUInt4(const char* label, unsigned int v[4], ImGuiInputTextFlags flags = 0);
+	bool InputUInt(const char* label,  unsigned int* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
+	bool InputUInt2(const char* label, unsigned int v[2], ImGuiInputTextFlags flags = 0);
+	bool InputUInt3(const char* label, unsigned int v[3], ImGuiInputTextFlags flags = 0);
+	bool InputUInt4(const char* label, unsigned int v[4], ImGuiInputTextFlags flags = 0);
 
-    bool ColorPreview3(const char* label, float  v[3], ImGuiColorEditFlags flags = 0);
-    inline bool ColorPreview3(const char* label, ImVec4 color, ImGuiColorEditFlags flags = 0) { ColorPreview3(label, &color.x, flags); }
-    inline bool ColorPreview3(const char* label, ImColor color, ImGuiColorEditFlags flags = 0) { ColorPreview3(label, &color.Value.x, flags); }
+	bool ColorPreview3(const char* label, float v[3], ImGuiColorEditFlags flags = 0);
+	inline bool ColorPreview3(const char* label, ImVec4 color, ImGuiColorEditFlags flags = 0) { ColorPreview3(label, &color.x, flags); }
+	inline bool ColorPreview3(const char* label, ImColor color, ImGuiColorEditFlags flags = 0) { ColorPreview3(label, &color.Value.x, flags); }
 
-    bool ColorPreview4(const char* label, float  v[4], ImGuiColorEditFlags flags = 0);
-    inline bool ColorPreview4(const char* label, ImVec4 color, ImGuiColorEditFlags flags = 0) { ColorPreview4(label, &color.x, flags); }
-    inline bool ColorPreview4(const char* label, ImColor color, ImGuiColorEditFlags flags = 0) { ColorPreview4(label, &color.Value.x, flags); }
+	bool ColorPreview4(const char* label, float v[4], ImGuiColorEditFlags flags = 0);
+	inline bool ColorPreview4(const char* label, ImVec4 color, ImGuiColorEditFlags flags = 0) { ColorPreview4(label, &color.x, flags); }
+	inline bool ColorPreview4(const char* label, ImColor color, ImGuiColorEditFlags flags = 0) { ColorPreview4(label, &color.Value.x, flags); }
 }
 
 #endif //IMGUI_EXTRAS_H
